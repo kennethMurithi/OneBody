2c435cac5f9925161f92e32baa515e3ea1e45294
[Kenneth Murithi] <[Kennethmmuriithi@gmail.com]>
2022-04-01 20:49:30 +0300

    Initial Project add

---
+++ src/test/java/com/onebody/churchoperations/ChurchOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebody.churchoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchOperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
