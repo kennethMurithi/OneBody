7d6928e47b7c06d4e44d3d3e65b5c79ca6d0dba1
[Kenneth Murithi] <[Kennethmmuriithi@gmail.com]>
2022-04-07 01:41:48 +0300

    adding Members index page and Controller. Closes #12

---
+++ onebody-web/src/main/java/churchoperations/controllers/MembersController.java
@@ -0,0 +1,14 @@
+package churchoperations.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MembersController {
+
+    @RequestMapping({"/members","/members/index","members/index.html"})
+    public String listMembers(){
+
+        return "/members/index";
+    }
+}
